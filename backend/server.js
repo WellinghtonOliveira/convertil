430627b3b35276a31becd59ce1faf45971201190
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-16 00:15:32 -0300

    a

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,6 @@ app.use(express.json())
 
 app.use('/', verificadorRoutes)
 
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,15 +1,15 @@
 const dados = [
     // desing
-    { category: 'design', name: 'Gerador de Paleta de Cores', description: 'Cria combinações de cores harmoniosas para projetos visuais', link: './public/gerador-de-paleta-de-cores/index.html'},
+    { category: 'design', name: 'Gerador de Paleta de Cores', description: 'Cria combinações de cores harmoniosas para projetos visuais', link: './public/gerador-de-paleta-de-cores/'},
 
     // dev
-    { category: 'dev', name: 'Formatador de Código', description: 'Organiza e melhora a legibilidade do código', link: './public/formatador-de-codigos/index.html'},
+    { category: 'dev', name: 'Formatador de Código', description: 'Organiza e melhora a legibilidade do código', link: './public/formatador-de-codigos/'},
 
     // productivity
-    { category: 'productivity', name: 'Cronômetro Online', description: 'Ideal para treinos, estudos, tarefas ou qualquer atividade, sem complicações.', link: './public/cronometro-online/index.html'},
+    { category: 'productivity', name: 'Cronômetro Online', description: 'Ideal para treinos, estudos, tarefas ou qualquer atividade, sem complicações.', link: './public/cronometro-online/'},
 
     // marketing
-    { category: 'marketing', name: 'Gerador de Títulos Atrativos', description: 'Crie títulos irresistíveis para seus vídeos que despertam emoção ou curiosidade.', link: './public/gerador-de-titulos/index.html' },
+    { category: 'marketing', name: 'Gerador de Títulos Atrativos', description: 'Crie títulos irresistíveis para seus vídeos que despertam emoção ou curiosidade.', link: './public/gerador-de-titulos/' },
 
     // finance
     //{ category: 'finance' }
