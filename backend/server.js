2720c9df338fb2b5cc2879578d7870c924d46d04
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-16 00:12:43 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,12 @@ app.use(express.json())
 
 app.use('/', verificadorRoutes)
 
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
 })
