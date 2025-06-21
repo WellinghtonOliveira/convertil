df8d042106a31e84cd45381be2220bfe19e86e7a
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-21 12:44:34 -0300

    Update utils.js

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,25 +1,25 @@
 const dados = [
     // design
-    { category: 'design', name: 'Gerador de Paleta de Cores', description: 'Cria combinações de cores harmoniosas para projetos visuais', link: './public/gerador-de-paleta-de-cores/' },
-    { category: 'design', name: 'Conversor de Cores HEX ⇄ RGB ⇄ HSL', description: 'Converte entre formatos de cor populares', link: './public/conversor-de-cores/' },
-    { category: 'design', name: 'Redimensionador de Imagem', description: 'Faz uma ateração limpa e proficional nas dimensões da imagem', link: './public/redimensionador-de-imagem' },
+    { category: 'design', name: 'Gerador de Paleta de Cores', description: 'Cria combinações de cores harmoniosas para projetos visuais', link: './gerador-de-paleta-de-cores/' },
+    { category: 'design', name: 'Conversor de Cores HEX ⇄ RGB ⇄ HSL', description: 'Converte entre formatos de cor populares', link: './conversor-de-cores/' },
+    { category: 'design', name: 'Redimensionador de Imagem', description: 'Faz uma ateração limpa e proficional nas dimensões da imagem', link: './redimensionador-de-imagem' },
 
     // dev
-    { category: 'dev', name: 'Formatador de Código', description: 'Organiza e melhora a legibilidade do código', link: './public/formatador-de-codigos/' },
-    { category: 'dev', name: 'Minificador de Código', description: 'Remove espaços e quebras de linha para otimizar o código', link: './public/minificador-de-codigos/' },
+    { category: 'dev', name: 'Formatador de Código', description: 'Organiza e melhora a legibilidade do código', link: './formatador-de-codigos/' },
+    { category: 'dev', name: 'Minificador de Código', description: 'Remove espaços e quebras de linha para otimizar o código', link: './minificador-de-codigos/' },
 
     // productivity
-    { category: 'productivity', name: 'Cronômetro Online', description: 'Ideal para treinos, estudos, tarefas ou qualquer atividade, sem complicações.', link: './public/cronometro-online/' },
-    { category: 'productivity', name: 'Bloco de Notas Online', description: 'Bloco de notas online: simples, rápido e prático. Faça anotações rápidas e salve sem complicação', link: './public/bloco-de-notas-online/' },
+    { category: 'productivity', name: 'Cronômetro Online', description: 'Ideal para treinos, estudos, tarefas ou qualquer atividade, sem complicações.', link: './cronometro-online/' },
+    { category: 'productivity', name: 'Bloco de Notas Online', description: 'Bloco de notas online: simples, rápido e prático. Faça anotações rápidas e salve sem complicação', link: './bloco-de-notas-online/' },
 
     // marketing
-    { category: 'marketing', name: 'Gerador de Títulos Atrativos', description: 'Crie títulos irresistíveis para seus vídeos que despertam emoção ou curiosidade.', link: './public/gerador-de-titulos/' },
-    { category: 'marketing', name: 'Contador de Caracteres e Palavras', description: 'Conta elementos de um texto: palavras, letras, espaços e linhas usadas.', link: './public/contador-de-caracteres-e-palavras/' },
+    { category: 'marketing', name: 'Gerador de Títulos Atrativos', description: 'Crie títulos irresistíveis para seus vídeos que despertam emoção ou curiosidade.', link: './gerador-de-titulos/' },
+    { category: 'marketing', name: 'Contador de Caracteres e Palavras', description: 'Conta elementos de um texto: palavras, letras, espaços e linhas usadas.', link: './contador-de-caracteres-e-palavras/' },
 
 
     // finance
-    { category: 'finance', name: 'Calculadora de Juros Compostos', description: 'Simule o crescimento do seu dinheiro com juros compostos e veja como seu capital pode evoluir ao longo do tempo. Ideal para planejar investimentos com aportes mensais e taxa fixa', link: './public/calculadora-de-juros-compostos/' },
-    { category: 'finance', name: 'Conversor de Moedas', description: 'Converte valores entre diferentes moedasConverta valores entre diferentes moedas do mundo com precisão e rapidez. Ferramenta online gratuita para conversão de câmbio.', link: './public/conversor-de-moedas/' },
+    { category: 'finance', name: 'Calculadora de Juros Compostos', description: 'Simule o crescimento do seu dinheiro com juros compostos e veja como seu capital pode evoluir ao longo do tempo. Ideal para planejar investimentos com aportes mensais e taxa fixa', link: './calculadora-de-juros-compostos/' },
+    { category: 'finance', name: 'Conversor de Moedas', description: 'Converte valores entre diferentes moedasConverta valores entre diferentes moedas do mundo com precisão e rapidez. Ferramenta online gratuita para conversão de câmbio.', link: './conversor-de-moedas/' },
 ]
 
 module.exports = dados
