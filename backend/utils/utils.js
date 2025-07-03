b3ef0e08724ba5fb1df38fd82bd0cb9268aa4721
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-07-03 08:42:45 -0300

    Atualização Correção

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -3,7 +3,7 @@ const dados = [
     { category: 'design', name: 'Gerador de Paleta de Cores', description: 'Cria combinações de cores harmoniosas para projetos visuais', link: './gerador-de-paleta-de-cores/' },
     { category: 'design', name: 'Conversor de Cores HEX ⇄ RGB ⇄ HSL', description: 'Converte entre formatos de cor populares', link: './conversor-de-cores/' },
     { category: 'design', name: 'Redimensionador de Imagem', description: 'Faz uma ateração limpa e proficional nas dimensões da imagem', link: './redimensionador-de-imagem' },
-    { category: 'design', name: 'Gerador de favicon', description: 'Crie um favicon para seu site - é rápido e fácil! envie sua própria imagem (SVG, JPG e PNG).', link: './gerador-de-favicon/' },
+    { category: 'design', name: 'Gerador de favicon', description: 'Crie um favicon para seu site - é rápido e fácil! envie sua própria imagem (SVG, JPG e PNG).', link: './criador-de-favicon/' },
     
     // dev
     { category: 'dev', name: 'Formatador de Código', description: 'Organiza e melhora a legibilidade do código', link: './formatador-de-codigos/' },
