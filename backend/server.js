c2db6476341ea87b38c02e0a27bb11072029c2f1
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-07-23 16:28:33 -0300

    atualização correção de bugs

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+
+
 app.use('/', verificadorRoutes)
 
 app.listen(PORT, () => {
